c61293dd14e901fe297c1ed14829f0a68359d0b7
0xheynacho <98292399+0xheynacho@users.noreply.github.com>
2022-07-19 15:19:22 -0400

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -32,8 +32,7 @@ export default function Dashboard() {
       </Head>
    <h1>si</h1>
 		  		
-		  <div ><div><div  style={{ color: 'blue'}} ><div><span>¿Qué Tipo de Cocina Te Gusta Más?</span></div></div></div></div>
-		  <input id="cocina3" type="radio" data-is-required="true" name="field3[]" defaultValue={26} />
+		<input id="cocina3" type="radio" data-is-required="true" name="field3[]" defaultValue={26} className="validation-lenient" />
 		  
 		  
 		  
