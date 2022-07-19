3b377848f2026a6c8314df49c4892b2d9b886d08
0xheynacho <98292399+0xheynacho@users.noreply.github.com>
2022-07-19 11:19:41 -0400

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -28,7 +28,16 @@ export default function Dashboard() {
       <Head>
         <title>Dashboard</title>
       </Head>
-
+	<div  class="casa">
+		<div style="text-align: center; color:black; width: 100%; opacity:2!important; margin-top:5%;" >
+			<h1 style="margin-bottom: 1%; margin-top: 3%;  font-family: 'Open Sans', sans-serif;" >Presupuesto de Diseño para   tu casa</h1>
+			<h2 style= color:black;font-family: 'Open Sans', sans-serif;" >Averiguá que Tamaño y costo Tendrá Tu Casa</h2>
+			<h2 style="margin-bottom: 1%;color:black;font-family: 'Open Sans', sans-serif;">Completando estos simples datos</h2>
+			<h2 style="color: black;font-family: 'Open Sans', sans-serif;">Obtén el precio de inmediato y descarga el PDF</h2>
+			<button   style="background-color: black ; color:white ; margin-top: 3%;" ><a href="#Presupuesto" style="color: white;"> Calcular  Presupusto</a>
+			</button>
+			</div>
+		</div>
       
     </Box>
   );
